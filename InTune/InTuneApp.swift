021720d45b0b1a5b3859dbc0f0395e4e1a77a90b
Nancy Luu <139261816+helloworldmynameisnancy@users.noreply.github.com>
2025-10-20 15:19:30 -0500

    Initial Commit

---
+++ InTune/InTuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InTuneApp.swift
+//  InTune
+//
+//  Created by Nancy Luu on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct InTuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
